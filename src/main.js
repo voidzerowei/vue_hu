1fcbdcf86190beaefdebefecca0275d1265b6644
voidzerowei <z5692794@gmail.com>
2023-09-26 14:03:42 +0800

    ✨ feat(调整代码): 基础搭建

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import store from "./store"; // 引入 Vuex store
+import router from "./router"; // 引入 Vue Router
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// 将 Vuex store 挂载到应用实例上
+app.use(store);
+
+// 将 Vue Router 挂载到应用实例上
+app.use(router);
+
+// 最后，挂载应用到 HTML 元素上
+app.mount("#app");
