dfdef138b49f5e64c84046e2d2edafd807d4f5f1
voidzerowei <z5692794@gmail.com>
2023-09-26 15:10:09 +0800

    ✨ feat(基础搭建): 路由bug

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import store from "./store"; // 引入 Vuex store
+// import store from "./store"; // 引入 Vuex store
 import router from "./router"; // 引入 Vue Router
 
 const app = createApp(App);
 
 // 将 Vuex store 挂载到应用实例上
-app.use(store);
+// app.use(store);
 
 // 将 Vue Router 挂载到应用实例上
 app.use(router);
